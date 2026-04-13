d0c80cb2a1305fa4d50087101b85247df050ee91
Midhuna S <midhunaaugust04_bec28@mepcoeng.ac.in>
2026-04-13 12:24:39 +0530

    Add files via upload

---
+++ python division.py
@@ -0,0 +1,16 @@
+python:division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Integer division
+    print(a // b)
+    
+    # Float division
+    print(a / b)
+
+
+
+
+
