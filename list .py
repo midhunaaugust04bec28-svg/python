445b21229234e434608012c254488c979fe81054
Midhuna S <midhunaaugust04_bec28@mepcoeng.ac.in>
2026-04-13 12:40:06 +0530

    Add files via upload

---
+++ list .py
@@ -0,0 +1,23 @@
+List Comprehensions
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    # Generate all coordinates [i, j, k] such that i+j+k != n
+    result = [[i, j, k] for i in range(x + 1) 
+                         for j in range(y + 1) 
+                         for k in range(z + 1) 
+                         if (i + j + k) != n]
+    
+    print(result)
+
+
+
+
+
+
+
+
